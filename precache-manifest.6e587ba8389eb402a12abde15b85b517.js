90af8efb98b36f7ff2ab7a0d3f66d3bcdb3c66a6
bar2104y <bar2104y@yandex.ru>
2019-08-13 11:38:08 +0300

    INIT

---
+++ precache-manifest.6e587ba8389eb402a12abde15b85b517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87ffd2d694545e7f85e",
+    "url": "/OranelaCalendar/static/js/runtime~main.33cfe3df.js"
+  },
+  {
+    "revision": "110c5c53bbbdf7305c61",
+    "url": "/OranelaCalendar/static/js/main.64c0cc3d.chunk.js"
+  },
+  {
+    "revision": "dfd61e34ee08b4f00e96",
+    "url": "/OranelaCalendar/static/js/2.5962b59b.chunk.js"
+  },
+  {
+    "revision": "dfd61e34ee08b4f00e96",
+    "url": "/OranelaCalendar/static/css/2.2673369c.chunk.css"
+  },
+  {
+    "revision": "0f69f3b40cee80d952bfa2aced10c107",
+    "url": "/OranelaCalendar/index.html"
+  }
+];
